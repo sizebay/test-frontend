74f03a81daf4da5c7d541068cac5fdc225129f30
Daniel Gustavo Favero <daniel.favero@nees.ufal.br>
2025-08-13 11:02:28 -0300

    feat: primeiro commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const openSans = Open_Sans({
+  weight: ["500", "400"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Repositórios Github",
+  description: "Procure repositórios de usuários do github",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt">
+      <body className={`${openSans.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
