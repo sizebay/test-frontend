246631c123a79aa8908d567dddf93f54be52281f
Daniel Gustavo Favero <daniel.favero@nees.ufal.br>
2025-08-17 19:15:19 -0300

    fix: corrige middleware de rota

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
+
+  if (url.pathname === "/login") return NextResponse.next();
+
   url.pathname = "/";
-  return NextResponse.rewrite(url);
+  return NextResponse.rewrite(new URL("/", request.url));
 }
 
 export const config = {
