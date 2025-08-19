90ffce94174e19f87482d0ef6ec91570fc70bd26
Daniel Gustavo Favero <daniel.favero@nees.ufal.br>
2025-08-18 21:56:42 -0300

    fix: altera texto do informativo de login

--- src/components/molecules/login-info-card.tsx
+++ src/components/molecules/login-info-card.tsx
@@ -1,5 +1,6 @@
 import { Info } from "lucide-react";
 import { Card, CardDescription, CardDetail, CardHeader } from "../atoms";
+import Link from "next/link";
 
 export function LoginInfoCard() {
   return (
@@ -10,8 +11,14 @@ export function LoginInfoCard() {
         </CardDetail>
       </CardHeader>
       <CardDescription>
-        O login não é necessário, apenas é recomendado no caso de diversos usos
-        devido ao limite de requisições da api do <i>Github</i>
+        Realizar login não é obrigatório, é apenas uma recomendação devido a
+        taxa de limite de requisições para IPs sem autenticação.{" "}
+        <Link
+          href="https://docs.github.com/pt/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28"
+          target="_blank"
+        >
+          <i>Entenda mais*</i>
+        </Link>
       </CardDescription>
     </Card>
   );
